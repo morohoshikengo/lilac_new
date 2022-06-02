de5c6c102801de1a8a1172e2c2ec245cc4bbb7e4
堂高哲 <dotaka@doukouakiranoMacBook-Air.local>
2022-06-02 23:24:13 +0900

    contact form

---
+++ script/contact.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    $('#form').submit(function (event) {
+      var formData = $('#form').serialize();
+      $.ajax({
+        url: "https://docs.google.com/forms/hogehogehttps://docs.google.com/forms/u/0/d/e/1FAIpQLSdOycpYzbe0XD33Us_R4nKBXCSf9NJpi3f8NS1Krh35ynS7XA/formResponse",
+        data: formData,
+        type: "POST",
+        dataType: "xml",
+        statusCode: {
+          0: function () {
+            $(".end-message").slideDown();
+            $(".submit-btn").fadeOut();
+            //window.location.href = "thanks.html";
+          },
+          200: function () {
+            $(".false-message").slideDown();
+          }
+        }
+      });
+      event.preventDefault();
+    });
+
+  });
