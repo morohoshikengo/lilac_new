f0c465a684e9dcb479b69e3de9da93640bc95c31
堂高哲 <dotaka@doukouakiranoMacBook-Air.local>
2022-10-06 01:43:48 +0900

    contactの修正

--- script/contact.js
+++ script/contact.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $('#form').submit(function (event) {
       var formData = $('#form').serialize();
       $.ajax({
-        url: "https://docs.google.com/forms/hogehogehttps://docs.google.com/forms/u/0/d/e/1FAIpQLSdOycpYzbe0XD33Us_R4nKBXCSf9NJpi3f8NS1Krh35ynS7XA/formResponse",
+        url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdOycpYzbe0XD33Us_R4nKBXCSf9NJpi3f8NS1Krh35ynS7XA/formResponse",
         data: formData,
         type: "POST",
         dataType: "xml",
